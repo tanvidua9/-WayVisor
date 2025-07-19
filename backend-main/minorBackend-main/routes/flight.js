0cba50b9d956625aad5b63bcbb10382965d3e3db
tanvidua9 <165177374+tanvidua9@users.noreply.github.com>
2025-07-19 22:11:55 +0530

    Update flight.js

--- backend-main/minorBackend-main/routes/flight.js
+++ backend-main/minorBackend-main/routes/flight.js
@@ -2,9 +2,6 @@ const express = require("express");
 const router = express.Router();
 const flightController = require("../controllers/flightController");
 
-router.get("/", (req, res, next) => {
-  console.log(`[INFO] API call received: ${req.originalUrl}`);
-  next();
-}, flightController.getFlights);
+router.get("/", flightController.getFlights);
 
 module.exports = router;
